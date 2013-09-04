4c675c9a65907dc980e26f32bd839e2099ee9ac0
Andrew Jorczak <andrew.jorczak@gmail.com>
2013-09-04 18:03:35 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AnattaLy::Application.config.secret_token = '5961db53aa32c61dc8746a6d94d42787c6d94b27bd4f1201ed9bd413e9d42297ad83984f31f59c7f7576f183dfe6a61989b9fd3775ad4dc540fbc7c0b37779f2'

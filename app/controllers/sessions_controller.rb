753f8cad9c26aa8f547c73e9a47c43b280565170
Andrew Jorczak <andrew.jorczak@gmail.com>
2013-09-05 10:22:05 -0500

    Added additional views for user model, sign in and sign out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
 	def create 
 		user = User.from_omniauth(env["omniauth.auth"])
-		session[:user_id] = user.user_id
+		session[:user_id] = user.user.id
 		redirect_to root_url
 	end
 

f570705d722d02f280d17e6cd174d4a8abaebb7b
Andrew Jorczak <andrew.jorczak@gmail.com>
2013-09-05 10:22:05 -0500

    added additional views for user model, sign in and sign out

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ActiveRecord::Base
-  attr_accessible :name
-
   def self.from_omniauth(auth)
   	where(auth.slice(:provider,:uid)).first_or_initialize.tap do |user|
   		user.provider = auth.provider

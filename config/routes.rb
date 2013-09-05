df8c7ee4caadb12d917903cfccb8cf6fd7b93951
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-05 15:53:51 -0500

    Added RESTful resources for Item and User

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ AnattaLy::Application.routes.draw do
 
   match 'auth/:provider/callback', to: 'sessions#create'
   match 'auth/failure', to: redirect('/')
-  match 'signout', to: 'sessions#destroy', as: 'signout'  
+  match 'signout', to: 'sessions#destroy', as: 'signout'
+
+  resources :users do
+    resources :items
+  end
 
   # The priority is based upon order of creation:
   # first created -> highest priority.

149ccf977e83a263e64dd79a2affa35e0a001b49
Apprentice <jvanstry@gmail.com>
2013-09-06 13:19:17 -0500

    Fixed changes on bracket spacing and syntax

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,12 +1,12 @@
 module UsersHelper
 
   def parse_facebook
-    facebook_ids = User.all.map {|user| user.facebook_id.to_s }
+    facebook_ids = User.all.map { |user| user.facebook_id.to_s }
     matches = []
 
     facebook_ids.each do |id|
-      matches << @friends.select{|friend| friend['id'] == id}
+      matches << @friends.select{ |friend| friend['id'] == id }
     end
-    matches.select{|item| item.any?}
+    matches.select{ |item| item.any? }
   end
 end

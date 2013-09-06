074959ae18edf87f3458e0fa24ce1934e8e69f37
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-06 10:45:53 -0500

    Added functionality for user to request another user as a share friend from a list of all users

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -3,4 +3,16 @@ class FriendsController < ApplicationController
     # Should only be user's possible friends
     @users = User.all
   end
+
+  def create
+    @friend = Friend.new
+    @friend.requester_id = 1 # should be current_user
+    @friend.receiver_id = params[:receiver_id]
+    @friend.save
+    redirect_to friend_url(@friend)
+  end
+
+  def show
+
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130905201736) do
+ActiveRecord::Schema.define(:version => 20130906015244) do
+
+  create_table "friends", :force => true do |t|
+    t.integer  "requester_id"
+    t.integer  "receiver_id"
+    t.boolean  "approved",     :default => false
+    t.datetime "created_at",                      :null => false
+    t.datetime "updated_at",                      :null => false
+  end
 
   create_table "items", :force => true do |t|
     t.string   "name"

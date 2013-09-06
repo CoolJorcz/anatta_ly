dc2733bf756b2dad4cd7485207803de52417412d
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-06 15:03:56 -0500

    Refactored UsersHelper to return User object instead of FQL return objects

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,12 +1,21 @@
 module UsersHelper
 
   def parse_facebook
+    # Find all the FB id's from anattaly Users
     facebook_ids = User.all.map { |user| user.facebook_id.to_s }
-    matches = []
 
+    # Select the matches between my FB friends and anattaly Users
+    matches = []
     facebook_ids.each do |id|
       matches << @friends.select{ |friend| friend['id'] == id }
     end
-    matches.select{ |item| item.any? }
+
+    # Remove empty arrays and make hash easier to use
+    user_matches = matches.select{ |item| item.any? }.map{ |match| match.pop }
+
+    # Return the anattaly User object for those members that match
+    anattaly_user_matches = []
+    user_matches.each{ |user| anattaly_user_matches << User.find_by_facebook_id(user["id"].to_i) }
+    anattaly_user_matches
   end
 end

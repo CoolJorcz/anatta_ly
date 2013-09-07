8cfb3c0c6f2e0329217a6d0bc3befddb7ec4aaf2
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-06 20:42:51 -0500

    Removed flash messages for re-renders

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,36 +21,33 @@ class ItemsController < ApplicationController
     @item.user_id = @user.id
 
     if @item.valid?
-      flash[:notice] = "Successful Create"
       @item.save
       redirect_to item_url(@item)
     else
-      flash[:notice] = "Failed validation"
       render :new
     end
   end
 
   def show
-    @item = Item.find_by_id(params[:id])
+    @item = Item.find(params[:id])
     @user = @item.user
   end
 
   def edit
-    @item = Item.find_by_id(params[:id])
+    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find_by_id(params[:id])
+    @item = Item.find(params[:id])
     if @item.update_attributes(params[:item])
       redirect_to item_url(@item)
     else
-      flash[:notice] = "Failed validation"
       render :edit
     end
   end
 
   def destroy
-    @item = Item.find_by_id(params[:id])
+    @item = Item.find(params[:id])
     @item.destroy
     redirect_to user_path(current_user.id)
   end

432a913b4c07fa3795190af118399b9f152de302
Andrew Jorczak <andrew.jorczak@gmail.com>
2013-09-07 16:27:50 -0500

    Changed Items error

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,5 +1,2 @@
 Paperclip::Attachment.default_options[:url] = 'http://anattaly.s3.amazonaws.com/'
-puts "*"*5000
-puts "We've made it here!"
-puts "*"*5000
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'

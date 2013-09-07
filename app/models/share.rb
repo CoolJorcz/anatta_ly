24073433080c1f0daa457ff2c0b6659d2a6ab297
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-07 15:44:33 -0500

    Added viewing of items share requests

--- app/models/share.rb
+++ app/models/share.rb
@@ -1,8 +1,23 @@
 class Share < ActiveRecord::Base
+  attr_accessible :item_id, :start_on, :end_on
   validates_presence_of :item_id, :borrower_id, :start_on, :end_on, :status
 
   belongs_to :item
   belongs_to :borrower, class_name: "User", foreign_key: "borrower_id"
+
+  def self.share_requests(current_user)
+    items = current_user.items
+    shares = []
+    items.each do |item|
+      shares << Share.where(item_id: item.id, status: "pending")
+    end
+
+    requests = []
+    shares.flatten.each do |share|
+      requests << Item.find(share.item_id)
+    end
+    requests
+  end
 
   def self.borrow_requests(current_user)
     shares = Share.where(borrower_id: current_user.id, status: "pending")

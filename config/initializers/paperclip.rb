6038df63821b2faa3664a7032cd4c887a92d3f44
Andrew Jorczak <andrew.jorczak@gmail.com>
2013-09-07 16:43:17 -0500

    Changes in the path url

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,2 +1,2 @@
 Paperclip::Attachment.default_options[:url] = 'http://anattaly.s3.amazonaws.com/'
-Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+Paperclip::Attachment.default_options[:path] = '/images/'

d12ff92cc63549c40935900d64cb8f7c3dfdcd66
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-07 11:36:00 -0500

    Updated class_name changes for associations

--- app/models/share.rb
+++ app/models/share.rb
@@ -2,5 +2,5 @@ class Share < ActiveRecord::Base
   validates_presence_of :item_id, :borrower_id, :start_on, :end_on, :status
 
   belongs_to :item
-  belongs_to :borrower, class_name "User"
+  belongs_to :borrower, class_name: "User", foreign_key: "borrower_id"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_many :items
+  has_many :borrows
   has_many :shares, through: :items
   has_many :friends, foreign_key: :requestor_id, foreign_key: :receiver_id
 

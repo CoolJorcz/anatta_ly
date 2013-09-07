a179fb4fe57f11fcf8cbce028134d615d75fe147
Andrew Jorczak <andrew.jorczak@gmail.com>
2013-09-07 11:24:33 -0500

    Added config prevention for Heroku asset precompiler failure

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module AnattaLy
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    #Adding in line to prevent Heroku failure on precompile
+    config.assets.initialize_on_precompile = false
   end
 end

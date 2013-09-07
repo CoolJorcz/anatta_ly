9b49ee736709a95686d3c3ddccc79a00c50dc8fa
Andrew Jorczak <andrew.jorczak@gmail.com>
2013-09-07 16:13:14 -0500

    Updated refs for paperclip s3 integration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,7 @@ AnattaLy::Application.configure do
   #For file uploads on Heroku
   config.paperclip_defaults = {
     :storage => :s3,
+    :s3_protocol => 'http',
     :s3_credentials => {
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],

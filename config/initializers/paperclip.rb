9be27d75bb21195ce166b3f6c77d1c33f2b76be6
Andrew Jorczak <andrew.jorczak@gmail.com>
2013-09-07 15:48:37 -0500

    Debugging code for heroku

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,2 +1,5 @@
 Paperclip::Attachment.default_options[:url] = 'http://anattaly.s3.amazonaws.com/'
+puts "*"*5000
+puts "We've made it here!"
+puts "*"*5000
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'

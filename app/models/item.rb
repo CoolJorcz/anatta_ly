2e231b19b2e744cd56d381924f17a4bc375ceb3c
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-07 21:47:11 -0500

    Going to bed

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,9 +3,9 @@ class Item < ActiveRecord::Base
   validates_presence_of :name, :description, :user_id
 
   belongs_to :user
-  has_many :shares
+  has_many :shares, dependent: :destroy
   has_many :borrowers, class_name: "User"
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :image, styles: { medium: "300x300>", small_sq: "200x200", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
 
   def self.items_of_friends(current_user)
     friends = Friend.get_friends(current_user)

b4e20bfdbff6ec3142509f6298f9f152d489090b
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-09 14:12:02 -0500

    Addded ability to update share information

--- app/models/share.rb
+++ app/models/share.rb
@@ -1,16 +1,9 @@
 class Share < ActiveRecord::Base
-  attr_accessible :item_id, :start_on, :end_on
+  attr_accessible :item_id, :start_on, :end_on, :pickup_location, :pickup_time, :return_location, :return_time
   validates_presence_of :item_id, :borrower_id, :start_on, :end_on, :status
 
   belongs_to :item
   belongs_to :borrower, class_name: "User", foreign_key: "borrower_id"
-
-  acts_as_gmappable
-
-  def gmaps4rails_address
-      #describe how to retrieve the address from your model, if you use directly a db column, you can dry your code, see wiki
-    "#{self.street}, #{self.city}, #{self.country}"
-  end
 
   def self.shares(current_user, status)
     items = current_user.items

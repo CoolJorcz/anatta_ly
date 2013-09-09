9c08a710a0600d65d326fb1d905dc4fa20916006
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-08 19:55:28 -0500

    Refactored borrowing and sharing capabilities and implemented index view

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,11 @@
 class WelcomeController < ApplicationController
 	def index
+    @friends = Friend.get_friends(current_user)
+    @items = []
+    @friends.each do |friend|
+      friend.items.each do |item|
+        @items << item
+      end
+    end
 	end
 end

c676a383ac0abe096027e527a6569b41f7663abe
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-10 10:41:14 -0500

    Added a seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,37 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
 
+def seed_category
+  categories = ['Apparel', 'Sports & Outdoors', 'Children', 'Books, Movies & Music', 'Electronics', 'Miscellaneous']
 
-
-categories = ['Apparel', 'Sports & Outdoors', 'Children', 'Books, Movies & Music', 'Electronics', 'Miscellaneous']
-
-categories.each do |category|
-	Category.create(name: category)
+  categories.each do |category|
+  	Category.create(name: category)
+  end
 end
+
+
+def new_item(user)
+  Item.create!(name: Faker::Lorem.word,
+           description: Faker::Lorem.sentence,
+           size: ['Large', 'Medium', 'Small'].sample,
+           color:['Red', 'Orange', "Yellow", "Blue"].sample,
+           category: Category.all.sample,
+           price: rand(1..100),
+           user: user,
+           image: File.new(Rails.root +
+            'public/system/items/images/000/000/016/medium/bigpreview_Red_canoe.jpg'))
+end
+
+def seed_items
+
+  10.times do
+    User.all.each do |user|
+      new_item(user)
+    end
+  end
+
+end
+
+seed_items

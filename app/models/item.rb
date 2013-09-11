a408566df06d503f7f3be0bd4d5dd515efaec49f
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-10 21:27:39 -0500

    Cleaned up commits per review

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ActiveRecord::Base
-  attr_accessible :name, :description, :size, :color, :image, :category, :price, :attachment, :user
+  attr_accessible :name, :description, :size, :color, :image, :category_id, :price, :attachment, :user
 
   validates_presence_of :name, :description, :user_id, :image_file_name, :category_id, :price
 

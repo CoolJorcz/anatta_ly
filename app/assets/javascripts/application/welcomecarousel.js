2d0ba5991890990794e7f7e3a4f022cda5c8c54c
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-12 17:23:25 -0500

    Working on styling of front page

---
+++ app/assets/javascripts/application/welcomecarousel.js
@@ -0,0 +1,17 @@
+$(function(){
+
+  $('#backstretch').backstretch([
+      "assets/index/test1.jpg",
+      "assets/index/test2.jpg",
+      "assets/index/test3.jpg",
+      "assets/index/test5.jpg",
+      "assets/index/test6.jpg",
+      "assets/index/test7.jpg",
+      "assets/index/test8.jpg",
+      "assets/index/test9.jpg",
+      "assets/index/test10.jpg",
+      "assets/index/test11.jpg",
+      "assets/index/test12.jpg",
+      "assets/index/test14.jpg",
+    ], {duration: 3000, fade: 750});
+});

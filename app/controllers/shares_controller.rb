d67707a0794690bd090410b24a6f8a344ed8f423
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-12 12:32:10 -0500

    Refactored naming on Share Panel for clarity

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -31,7 +31,7 @@ class SharesController < ApplicationController
     if end_on < start_on || start_on < today
       redirect_to new_share_path(item_id: item_id)
     elsif share.save
-      redirect_to root_url
+      redirect_to shares_url
     else
       redirect_to new_share_path(item_id: item_id)
     end
@@ -84,15 +84,10 @@ class SharesController < ApplicationController
       else
         redirect_to share_path(@share)
       end
-      if request.xhr?
-        render json: @share
-      else
-        redirect_to root_path
-      end
     else
       flash[:notice] = "Failed share approval"
-      render "welcome#index"
     end
+    redirect_to share_path(@share)
   end
 
   def destroy
